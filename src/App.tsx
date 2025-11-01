cb029e452bb3664407c2e6702d91c7154828c6ff
Vasu Dhall <gamedhall@gmail.com>
2025-11-01 20:59:35 +0530

    Fixed race condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useEffect } from "react";
 import "./App.css";
 import Landing from "./components/Landing/Landing";
-import { createLocalStorage } from "./utility/localStorage";
 
 function App() {
-    useEffect(() => {
-        createLocalStorage();
-    }, []);
     return (
         <>
             <Landing></Landing>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import UserProvider from "./components/UserProvider/UserProvider.tsx";
-
+import { createLocalStorage } from "./utility/localStorage.ts";
+createLocalStorage();
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <UserProvider>
